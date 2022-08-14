b7930bf88f1b670df092766b7b2acf027aa94e21
Mike Peiman <mikepeiman@gmail.com>
2022-08-14 13:42:48 -0400

    fixed color scheme on skills

--- src/routes/resume/ProjectSkills.svelte
+++ src/routes/resume/ProjectSkills.svelte
@@ -5,10 +5,10 @@
 <div class="flex flex-col text-xs flex-wrap">
 	<div class="tech-list flex flex-wrap my-2 text-warmGray-200">
 		{#each project.technologies_used as tech}
-			<div class="mr-1 mb-1 px-[3px] py-[1px] bg-blue-900 rounded-sm bg-opacity-80">{tech}</div>
+			<div class="mr-1 mb-1 px-[3px] py-[1px] bg-winterblues-700 rounded-sm">{tech}</div>
 		{/each}
 		{#each project.skills_applied as skill}
-			<div class="mr-1 mb-1 px-[3px] py-[1px] bg-limegreen-800 bg-opacity-60 rounded-sm">
+			<div class="mr-1 mb-1 px-[3px] py-[1px] bg-fuchsia-800  rounded-sm">
 				{skill}
 			</div>
 		{/each}
