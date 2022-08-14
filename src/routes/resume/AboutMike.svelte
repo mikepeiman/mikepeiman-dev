8869edf38c7ec3839f3cbfc99ddf7e4fa0f9bab1
Mike Peiman <mikepeiman@gmail.com>
2022-08-14 16:52:17 -0400

    added more personality to About Me

--- src/routes/resume/AboutMike.svelte
+++ src/routes/resume/AboutMike.svelte
@@ -3,15 +3,21 @@
 </script>
 <div class=" flex flex-col items-start text-xs justify-around m-2 w-full">
     <div class="flex h-auto w-full border-b-2 border-winterblues-700  ">
-        <h3 class=" text-gray-800 p-0 pl-2 text-lg">Unique Me</h3>
+        <h3 class=" text-gray-800 p-0 pl-2 text-lg">About Me</h3>
     </div>
     <div class="flex flex-wrap my-2">
-        Single parent, lifelong learner, philosopher. I bring a focus always on solutions and improvements, with a passion for learning, collaborating and creating.
+        Single parent, lifelong learner, philosopher. I'm always looking for solutions and improvements, with a passion for learning, collaborating and creating.
     </div>
     <div class="flex flex-wrap my-2">
         Gifted in language, critical thinking, and teaching.
     </div>
     <div class="flex flex-wrap my-2">
-        Hobbies & passions include fitness, snowboarding, music, nature, and gaming.
+        Hobbies & passions include fitness, snowboarding, playing piano, mushroom foraging, and gaming.
+    </div>
+    <div class="flex flex-wrap my-2">
+        Fun fact: I'm an expert snowboard instructor, and have myself certified approx. 80 new snowboard instructors. I also spent several years as a CrossFit trainer. These have given me invaluable experience and insight into teaching and pedagogy.
+    </div>
+    <div class="flex flex-wrap my-2">
+        Fun fact #2: My daughter was conceived in Ghana, West Africa, where I worked as Operations Manager & Systems Designer for Ghana's first commercial aquaponics farm.
     </div>
 </div>
