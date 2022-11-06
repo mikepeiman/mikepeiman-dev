a2a90b3b545a8f8c8313eb8ecaa0c47b7f846d7f
Mike Peiman <mikepeiman@gmail.com>
2022-11-06 10:48:56 -0500

    made project URLs clickable links, open in new tab

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -34,40 +34,87 @@
 				>
 					Home
 				</h1>
-			</div>	
+			</div>
 		</a>
 	</div>
 	<div class="flex flex-col w-full h-full align-center justify-center text-center">
-		<h1 class="text-6xl text-[2.5rem] sm:text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-display">Projects</h1>
+		<h1
+			class="text-6xl text-[2.5rem] sm:text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-display"
+		>
+			Projects
+		</h1>
 		<div
 			class="flex flex-col py-8 my-4 w-full bg-black bg-opacity-25 text-xl md:text-2xl lg:text-3xl xl:text-4xl font-montserrat"
 		>
 			{#each projects as project, i}
-				<div class="project relative p-8 my-6 border-l-8 border-cyan-600 rounded-r-md bg-warmGray-900 grid grid-cols-12 items-start">
+				<div
+					class="project relative p-8 my-6 border-l-8 border-cyan-600 rounded-r-md bg-warmGray-900 grid grid-cols-12 items-start"
+				>
 					<div class="flex col-span-12 mb-2 items-start justify-start text-left flex-wrap w-full">
-						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">Type: <div class="bg-yellow-700 bg-opacity-40 text-gray-300 rounded-sm px-2 ml-1 py-0">{project.type}</div></div>
-						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">Genre: <div class="bg-yellow-600 bg-opacity-40 text-gray-300 rounded-sm px-2 ml-1 py-0">{project.genre}</div></div>
-						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">Purpose: <div class="bg-yellow-500 bg-opacity-40 text-gray-300 rounded-sm px-1 ml-2 py-0">{project.purpose}</div></div>
+						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">
+							Type: <div
+								class="bg-yellow-700 bg-opacity-40 text-gray-300 rounded-sm px-2 ml-1 py-0"
+							>
+								{project.type}
+							</div>
+						</div>
+						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">
+							Genre: <div
+								class="bg-yellow-600 bg-opacity-40 text-gray-300 rounded-sm px-2 ml-1 py-0"
+							>
+								{project.genre}
+							</div>
+						</div>
+						<div class="flex metadata text-sm mr-4  text-gray-300 rounded-sm px-2 py-0">
+							Purpose: <div
+								class="bg-yellow-500 bg-opacity-40 text-gray-300 rounded-sm px-1 ml-2 py-0"
+							>
+								{project.purpose}
+							</div>
+						</div>
 					</div>
 					<div class="flex flex-col col-span-7 text-left w-full">
 						<h2 class=" w-full border-b-2 border-cyan-300 mb-6 pb-0 h-fit">{i} {project.name}</h2>
-						<div class="flex text-sm font-sans">{project.description.full.length ? project.description.full : project.description.stub + ' ' + project.description.brief}</div>
+						<div class="flex text-sm font-sans">
+							{project.description.full.length
+								? project.description.full
+								: project.description.stub + ' ' + project.description.brief}
+						</div>
 					</div>
-					<div class="flex flex-col col-span-5 w-full h-auto items-end justify-start border-r-4 border-cyan-600 bg-opacity-40">
-						<div class="flex metadata text-sm  text-gray-300 rounded-sm "><div class=" text-gray-300 rounded-sm p-1">{project.date_single}</div></div>
-						<div class="flex metadata text-sm  text-gray-300 rounded-sm "><div class=" text-cyan-400 rounded-sm px-1  py-0">{project.short_url}</div></div>
-						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">{#each project.technologies_used as tech}<div class="bg-fuchsia-500 bg-opacity-30 m-1 text-gray-300 rounded-sm px-1  py-0">{tech}</div>{/each}</div>
-						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">{#each project.skills_applied as skill}<div class="bg-yellow-500 bg-opacity-30 m-1 text-gray-300 rounded-sm px-1  py-0">{skill}</div>{/each}</div>
+					<div
+						class="flex flex-col col-span-5 w-full h-auto items-end justify-start border-r-4 border-cyan-600 bg-opacity-40"
+					>
+						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">
+							<div class=" text-gray-300 rounded-sm p-1">{project.date_single}</div>
+						</div>
+						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">
+							<a href="{project.deployment_url}" target="_blank" class="hover:decoration-amber-500 border-b-[1px] border-transparent hover:border-cyan-600">
+								<div class=" text-cyan-400 rounded-sm px-1  py-0">{project.short_url}</div>
+							</a>
+						</div>
+						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">
+							{#each project.technologies_used as tech}<div
+									class="bg-fuchsia-500 bg-opacity-30 m-1 text-gray-300 rounded-sm px-1  py-0"
+								>
+									{tech}
+								</div>{/each}
+						</div>
+						<div class="flex metadata text-sm  text-gray-300 rounded-sm ">
+							{#each project.skills_applied as skill}<div
+									class="bg-yellow-500 bg-opacity-30 m-1 text-gray-300 rounded-sm px-1  py-0"
+								>
+									{skill}
+								</div>{/each}
+						</div>
 					</div>
 				</div>
 			{/each}
 		</div>
 	</div>
-
 </div>
 
 <style lang="scss">
-.projects {
-	// max-width: 60rem;
-}
+	.projects {
+		// max-width: 60rem;
+	}
 </style>
